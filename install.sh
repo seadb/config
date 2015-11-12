84e668aa37955eb1e70cac6b60752a4e57c238c8
Chelsea Bridson <bridsonc@msu.edu>
2015-11-12 21:20:28 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-ln -s ~/.config/.vimrc ~/.vimrc
-ln -s ~/.config/.gitconfig ~/.gitconfig
-ln -s ~/.config/.bash_profile ~/.bash_profile
-ln -s ~/.config/.vim ~/.vim
+ln -s ~/.config/.vimrc ~/.vimrc -f
+ln -s ~/.config/.gitconfig ~/.gitconfig -f
+ln -s ~/.config/.bash_profile ~/.bash_profile -f
+ln -s ~/.config/.vim ~/.vim -f
