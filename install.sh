27b701016e1584af1f3f23f35691cedd1d191a7c
Chelsea Bridson <bridsonc@msu.edu>
2016-01-03 22:06:53 +0000

    renamed files, updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-ln -s ~/.config/.vimrc ~/.vimrc -f
-ln -s ~/.config/.gitconfig ~/.gitconfig -f
-ln -s ~/.config/.bash_profile ~/.bash_profile -f
+ln -s ~/.config/vimrc ~/.vimrc -f
+ln -s ~/.config/git-config ~/.gitconfig -f
+ln -s ~/.config/.gitignore ~/.gitignore -f
+ln -s ~/.config/bash-profile ~/.bash_profile -f
 source ~/.bash_profile
-ln -s ~/.config/.vim ~/.vim -f
-ln -s ~/.config/.tmux.conf ~/.tmux.conf -f
-ln -s ~/.config/config ~/.tmux.conf -f
+ln -s ~/.config/vim ~/.vim -f
+ln -s ~/.config/tmux-conf ~/.tmux.conf -f
+ln -s ~/.config/ssh-config ~/.ssh/config -f
