a4addfd78b4fb6e5454204c7e00c576dcbfbe6ef
Chelsea Bridson <bridsonc@msu.edu>
2016-01-03 22:07:45 +0000

    renamed gitignore and updated build script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -s ~/.config/vimrc ~/.vimrc -f
 ln -s ~/.config/git-config ~/.gitconfig -f
-ln -s ~/.config/.gitignore ~/.gitignore -f
+ln -s ~/.config/git-ignore ~/.gitignore -f
 ln -s ~/.config/bash-profile ~/.bash_profile -f
 source ~/.bash_profile
 ln -s ~/.config/vim ~/.vim -f
