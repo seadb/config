d516277a50c9adec9dc7ab51f523f6b2f718fcd6
Chelsea Bridson <bridsonc@msu.edu>
2016-01-08 00:00:05 -0500

    fixed weirdness with .vim folder

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-ln -sf ~/.config/vimrc ~/.vimrc
-ln -sf ~/.config/git-config ~/.gitconfig
-ln -sf ~/.config/git-ignore ~/.gitignore
-ln -sf ~/.config/bash-profile ~/.bash_profile
+ln -sfv ~/.config/vimrc ~/.vimrc
+ln -sfv ~/.config/git-config ~/.gitconfig
+ln -sfv ~/.config/git-ignore ~/.gitignore
+ln -sfv ~/.config/bash-profile ~/.bash_profile
+# if ~/.vim already exists then delete it
+if [ -d ~/.vim ]; then
+  rm -rf ~/.vim
+fi
+ln -sfv ~/.config/vim ~/.vim
+ln -sfv ~/.config/tmux-conf ~/.tmux.conf
+ln -sfv ~/.config/ssh-config ~/.ssh/config
 source ~/.bash_profile
-ln -sf ~/.config/vim ~/.vim
-ln -sf ~/.config/tmux-conf ~/.tmux.conf
-ln -sf ~/.config/ssh-config ~/.ssh/config
